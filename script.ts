1184c614b78da42c63d53ee61e98945ed74ff858
sigristarisa <arisasigrist.ch@gmail.com>
2022-08-15 16:38:05 +0200

    initial commit

---
+++ script.ts
@@ -0,0 +1,18 @@
+let weatherData: {
+  weather: object[];
+  main: {
+    temp: number;
+    feels_like: number;
+    temp_min: number;
+    temp_max: number;
+    pressure: number;
+    humidity: number;
+  };
+  wind: {
+    speed: number;
+  };
+  sys: {
+    country: string;
+  };
+  name: string;
+};
